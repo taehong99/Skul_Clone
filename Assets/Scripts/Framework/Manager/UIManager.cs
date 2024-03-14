85360ecd58d73635b5c51ae075e5084ae3b2a12e
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-03-14 14:20:29 +0900

    feat : PauseUI + InventoryUI

--- Assets/Scripts/Framework/Manager/UIManager.cs
+++ Assets/Scripts/Framework/Manager/UIManager.cs
@@ -1,7 +1,9 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class UIManager : Singleton<UIManager>
 {
@@ -12,13 +14,69 @@ public class UIManager : Singleton<UIManager>
     [SerializeField] Image popUpBlocker;
     [SerializeField] Button inGameBlocker;
 
+    private Dictionary<string, BaseUI> dictionary = new Dictionary<string, BaseUI>();
+
     private Stack<PopUpUI> popUpStack = new Stack<PopUpUI>();
     private float prevTimeScale;
     private InGameUI curInGameUI;
+    private PopUpUI curPopUpUI;
 
     private void Start()
     {
         EnsureEventSystem();
+    }
+
+    private void Update()
+    {
+        if (curPopUpUI != null && curPopUpUI.name != "InventoryUI")
+            return;
+
+        if (Keyboard.current[Key.D].wasPressedThisFrame)
+        {
+            Debug.Log("D Pressed this frame");
+            ShowPopUpUI<DescriptionUI>();
+        }
+        else if (Keyboard.current[Key.D].wasReleasedThisFrame)
+        {
+            Debug.Log("D released this frame");
+            ClosePopUpUI();
+        }
+    }
+
+    private void OnEscape()
+    {
+        if (curPopUpUI == null)
+        {
+            ShowPopUpUI<PauseUI>();
+            curPopUpUI = Load<PauseUI>($"UI/PopUp/PauseUI");
+        }
+        else if (curPopUpUI.name == "PauseUI")
+        {
+            ClosePopUpUI();
+            curPopUpUI = null;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void OnTab()
+    {
+        if (curPopUpUI == null)
+        {
+            ShowPopUpUI<InventoryUI>();
+            curPopUpUI = Load<InventoryUI>($"UI/PopUp/InventoryUI");
+        }
+        else if (curPopUpUI.name == "InventoryUI")
+        {
+            ClosePopUpUI();
+            curPopUpUI = null;
+        }
+        else
+        {
+            return;
+        }
     }
 
     public void EnsureEventSystem()
@@ -35,7 +93,7 @@ public class UIManager : Singleton<UIManager>
         if (popUpStack.Count > 0)
         {
             PopUpUI topUI = popUpStack.Peek();
-            topUI.gameObject.SetActive(false);
+            //topUI.gameObject.SetActive(false);
         }
         else
         {
@@ -47,6 +105,18 @@ public class UIManager : Singleton<UIManager>
         T ui = Instantiate(popUpUI, popUpCanvas.transform);
         popUpStack.Push(ui);
         return ui;
+    }
+
+    public T ShowPopUpUI<T>(string path) where T : PopUpUI
+    {
+        T resource = Load<T>($"UI/PopUp/{path}");
+        return ShowPopUpUI(resource);
+    }
+
+    public T ShowPopUpUI<T>() where T : PopUpUI
+    {
+        T resource = Load<T>($"UI/PopUp/{typeof(T).Name}");
+        return ShowPopUpUI(resource);
     }
 
     public void ClosePopUpUI()
@@ -118,5 +188,19 @@ public class UIManager : Singleton<UIManager>
         inGameBlocker.gameObject.SetActive(false);
         Destroy(curInGameUI.gameObject);
         curInGameUI = null;
+    }
+
+    private T Load<T>(string path) where T : BaseUI
+    {
+        if (dictionary.TryGetValue(path, out BaseUI ui))
+        {
+            return ui as T;
+        }
+        else
+        {
+            T resource = Resources.Load<T>(path);
+            dictionary.Add(path, resource);
+            return resource;
+        }
     }
 }
