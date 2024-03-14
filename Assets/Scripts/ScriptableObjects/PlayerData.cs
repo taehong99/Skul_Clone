1d22f9ae05e55e2b47da5a9652771015d4291607
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-03-14 23:48:17 +0900

    feat : random changes

---
+++ Assets/Scripts/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Player", fileName = "PlayerData")]
+public class PlayerData : ScriptableObject
+{
+    [Header("Player Stats")]
+    public int baseHP;
+    public int baseAttack;
+    public float moveSpeed;
+    public int dashCount;
+
+    [Header("Skull-Specific Stats")]
+    public RuntimeAnimatorController animatorController;
+    public float skill1Cooldown;
+    public float skill2Cooldown;
+
+    [Header("Icons")]
+    public Sprite skullIcon;
+    public Sprite skill1Icon;
+    public Sprite skill2Icon;
+
+    [Header("Texts")]
+    public string skullName;
+    public string type;
+    public string rarity;
+    public string swapTitle;
+    public string swapDescription;
+    public string skill1Name;
+    public string skill1Description;
+    public string skill2Name;
+    public string skill2Description;
+}
